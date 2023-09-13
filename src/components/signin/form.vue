7d7d2007e9f01dddac1b16f12746eb9a0a0b44b4
nima.khdb <n.khodabakhshi13@gmail.com>
2023-09-14 01:05:56 +0330

    transform from option to composition in sigin form component

--- src/components/signin/form.vue
+++ src/components/signin/form.vue
@@ -53,110 +53,101 @@
   </v-sheet>
 </template>
 
-<script lang="ts">
-import rules from "../../assets/rules";
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import _rules from "../../assets/rules";
+// props: ["title"],
+const props = defineProps({
+  title: String,
+});
+let rules = ref(_rules) as any;
+let detail = ref({}) as any;
+let cardSides = [
+  {
+    title: "SignUp",
+    isFormValid: false,
+    fields: [
+      {
+        title: "UserName",
+        value: "",
+        type: "text",
+        rules: "username",
+        icon: "fa-solid fa-user",
+      },
+      {
+        title: "Email",
+        value: "",
+        type: "text",
+        rules: "email",
+        icon: "fa-solid fa-envelope",
+      },
+      {
+        title: "Phone Number",
+        value: "",
+        type: "tel",
+        rules: "phone",
+        icon: "fa-solid fa-phone",
+      },
+      {
+        title: "Password",
+        value: "",
+        type: "password",
+        rules: "password",
+        icon: "fa-solid fa-key",
+      },
+    ],
+    spanText: "do you have an account?",
+    btnText1: "login",
+    btnText2: "signup",
+  },
+  {
+    title: "LogIn",
+    isFormValid: false,
+    fields: [
+      {
+        title: "UserName",
+        value: "",
+        type: "text",
+        rules: "required",
+        icon: "fa-solid fa-user",
+      },
+      {
+        title: "Password",
+        value: "",
+        type: "password",
+        rules: "required",
+        icon: "fa-solid fa-key",
+      },
+    ],
+    spanText: "don't you have an account?",
+    btnText1: "signup",
+    btnText2: "login",
+  },
+];
+let loading = ref(false);
 
-export default {
-  props: ["title"],
-  emits: ["flip"],
-  data() {
-    return {
-      rules: {} as any,
-      detail: {} as any,
-      cardSides: [
-        {
-          title: "SignUp",
-          isFormValid: false,
-          fields: [
-            {
-              title: "UserName",
-              value: "",
-              type: "text",
-              rules: "username",
-              icon: "fa-solid fa-user",
-            },
-            {
-              title: "Email",
-              value: "",
-              type: "text",
-              rules: "email",
-              icon: "fa-solid fa-envelope",
-            },
-            {
-              title: "Phone Number",
-              value: "",
-              type: "tel",
-              rules: "phone",
-              icon: "fa-solid fa-phone",
-            },
-            {
-              title: "Password",
-              value: "",
-              type: "password",
-              rules: "password",
-              icon: "fa-solid fa-key",
-            },
-          ],
-          spanText: "do you have an account?",
-          btnText1: "login",
-          btnText2: "signup",
-        },
-        {
-          title: "LogIn",
-          isFormValid: false,
-          fields: [
-            {
-              title: "UserName",
-              value: "",
-              type: "text",
-              rules: "required",
-              icon: "fa-solid fa-user",
-            },
-            {
-              title: "Password",
-              value: "",
-              type: "password",
-              rules: "required",
-              icon: "fa-solid fa-key",
-            },
-          ],
-          spanText: "don't you have an account?",
-          btnText1: "signup",
-          btnText2: "login",
-        },
-      ],
-      loading: false,
-    };
-  },
+function submit() {
+  if (detail.value.isFormValid) {
+    loading.value = true;
+    for (let item of detail.fields) console.log(item.value);
+  }
+}
+function checkPhoneIsNumber(event: any) {
+  if (event.target.type == "tel") {
+    var charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46) {
+      event.preventDefault();
+    } else {
+      return true;
+    }
+  }
+}
 
-  methods: {
-    submit() {
-      if (this.detail.isFormValid) {
-        this.loading = true;
-        for (let item of this.detail.fields) console.log(item.value);
-      }
-    },
-    checkPhoneIsNumber(event: any) {
-      if (event.target.type == "tel") {
-        var charCode = event.which ? event.which : event.keyCode;
-        if (
-          charCode > 31 &&
-          (charCode < 48 || charCode > 57) &&
-          charCode !== 46
-        ) {
-          event.preventDefault();
-        } else {
-          return true;
-        }
-      }
-    },
-  },
-  mounted() {
-    this.rules = rules;
-    if (this.title == "signup") this.detail = this.cardSides[0];
-    else this.detail = this.cardSides[1];
-  },
-};
+onMounted(() => {
+  console.log(cardSides);
+  if (props.title == "signup") detail.value = cardSides[0];
+  else detail.value = cardSides[1];
+});
 </script>
 
 <style>
