fe2f6e45fbe2127058cb54b8106bd3c55106cabf
nima.khdb <n.khodabakhshi13@gmail.com>
2023-09-13 23:43:56 +0330

    .env file added

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,27 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import 'vuetify/styles'
-import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
-
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { aliases, fa } from 'vuetify/iconsets/fa'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "vuetify/styles";
+import "@fortawesome/fontawesome-free/css/all.css"; // Ensure your project is capable of handling css files
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, fa } from "vuetify/iconsets/fa";
 
 const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'fa',
-        aliases,
-        sets: {
-          fa,
-        },
-      },
-})
+  components,
+  directives,
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
 
-const app = createApp(App)
-
-app.use(router)
-
-app.use(vuetify).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
