b504a3a51c1b1be6843664b9633b99f7140fbf6a
nima.khdb <n.khodabakhshi13@gmail.com>
2023-09-14 17:21:42 +0330

    baseApi set get method

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,8 @@
   <router-view></router-view>
 </template>
 
-<script lang="ts">
-import { BaseApiService } from "./plugins/axios";
-export default {
-  mounted() {
-    const api = new BaseApiService();
-  },
-};
+<script lang="ts" setup>
+
 </script>
 
 <style></style>
--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,14 +1,36 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
-export class BaseApiService {
+import { config } from 'dotenv'
+export class BaseApi {
     private api: AxiosInstance
     protected authorize: boolean = true
   
     public constructor() {
       this.api = axios.create()
-      this.api.defaults.baseURL = import.meta.env.VITE_API_URL_
+      this.api.defaults.baseURL = import.meta.env.VITE_API_URL
     }
 
     public get(url: string){
+      let config = {
+        headers : {}
+      } as AxiosRequestConfig
+      config = this.appendAuth(config)
+      return this.api.get(url, config).then(undefined)
+    }
 
+    public post(url: string, data: object){
+      let config = {
+        headers : {}
+      } as AxiosRequestConfig
+      config = this.appendAuth(config)
+      console.log()
+      return this.api.post(url, data, config).then(undefined)
+    }
+
+    private appendAuth(config: AxiosRequestConfig){
+      if (!this.authorize) return config
+      const token = localStorage.getItem('token')
+      if(config.headers)
+        config.headers.Authorization = `Bearer ${token}`
+      return config
     }
 }
