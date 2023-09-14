a505974828e25485295b3b08d27418d8453c80f2
nima.khdb <n.khodabakhshi13@gmail.com>
2023-09-14 17:22:59 +0330

    baseApi authAppend

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,29 +1,38 @@
-import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
-import { config } from 'dotenv'
+import axios, {
+  type AxiosInstance,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from "axios";
+import { config } from "dotenv";
 export class BaseApi {
-    private api: AxiosInstance
-    protected authorize: boolean = true
-  
-    public constructor() {
-      this.api = axios.create()
-      this.api.defaults.baseURL = import.meta.env.VITE_API_URL
-    }
+  private api: AxiosInstance;
+  protected authorize: boolean = true;
 
-    public get(url: string){
-      let config = {
-        headers : {}
-      } as AxiosRequestConfig
-      return this.api.get(url, config).then(undefined)
-    }
+  public constructor() {
+    this.api = axios.create();
+    this.api.defaults.baseURL = import.meta.env.VITE_API_URL;
+  }
 
-    public post(url: string, data: object){
-      let config = {
-        headers : {}
-      } as AxiosRequestConfig
-      console.log()
-      return this.api.post(url, data, config).then(undefined)
-    }
+  public get(url: string) {
+    let config = {
+      headers: {},
+    } as AxiosRequestConfig;
+    config = this.appendAuth(config);
+    return this.api.get(url, config).then(undefined);
+  }
 
+  public post(url: string, data: object) {
+    let config = {
+      headers: {},
+    } as AxiosRequestConfig;
+    config = this.appendAuth(config);
+    return this.api.post(url, data, config).then(undefined);
+  }
 
-
+  private appendAuth(config: AxiosRequestConfig) {
+    if (!this.authorize) return config;
+    const token = localStorage.getItem("token");
+    if (config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  }
 }
