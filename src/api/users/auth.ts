a034741f1c84ef47ceeceffeefd503a0228e31d6
nima.khdb <n.khodabakhshi13@gmail.com>
2023-09-14 20:17:44 +0330

    user login api

--- src/api/users/auth.ts
+++ src/api/users/auth.ts
@@ -1,7 +1,13 @@
 import { BaseApi } from "@/plugins/axios";
 export class AuthApi extends BaseApi {
+  authorize = false;
   async signup(userData: object) {
-    const response = await this.post("/users/signup/", userData);
+    const response = await this.post("/users/signup", userData);
+    return response?.data;
+  }
+
+  async login(userData: object) {
+    const response = await this.post("/users/login", userData);
     return response?.data;
   }
 }
