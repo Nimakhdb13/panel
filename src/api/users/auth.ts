2b05af61ca44f3c3478ca1a22c986743ff9014dd
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-20 19:39:58 +0330

    baseUrl added to api sript

--- src/api/users/auth.ts
+++ src/api/users/auth.ts
@@ -1,13 +1,14 @@
 import { BaseApi } from "@/plugins/axios";
 export class AuthApi extends BaseApi {
   authorize = false;
+  baseUrl = '/users/' 
   async signup(userData: object) {
-    const response = await this.post("/users/signup", userData);
+    const response = await this.post(this.baseUrl + "signup", userData);
     return response?.data;
   }
 
   async login(userData: object) {
-    const response = await this.post("/users/login", userData);
+    const response = await this.post(this.baseUrl + "login", userData);
     return response?.data;
   }
 }
