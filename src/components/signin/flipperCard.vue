857486d6a9d7f59c0398c7f9762d177eb5ad85c2
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-22 16:14:35 +0330

    delete alert and routing when submit in fleppercard comp.

--- src/components/signin/flipperCard.vue
+++ src/components/signin/flipperCard.vue
@@ -5,7 +5,6 @@
         <div class="flip-card-front">
           <v-card class="bg-blue-lighte-5 elevation-8">
             <SignInForm
-              @submit="(res) => submited(res, true)"
               @flip="flip('login')"
               title="signup"
             />
@@ -14,7 +13,6 @@
 
         <v-card class="flip-card-back">
           <SignInForm
-            @submit="(res) => submited(res, false)"
             @flip="flip('signup')"
             title="login"
           />
@@ -22,41 +20,13 @@
       </div>
     </div>
   </div>
-  <v-fade-transition>
-    <v-alert
-      density="comfortable"
-      class="fix-b mb-10 w-20"
-      closable
-      v-if="showAlert"
-      :type="(response.type as any)"
-      :value="false"
-      elevation="10"
-    >
-      {{ response.text }}
-    </v-alert>
-  </v-fade-transition>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 import SignInForm from "./form.vue";
-import router from "@/router";
-let showAlert = ref(false);
-let response = ref({ type: "", text: "" });
 let flipClass = ref("");
-function submited(res: any, isSignup: boolean) {
-  response.value = {
-    type: res.success ? "success" : "error",
-    text: res.success
-      ? `${isSignup ? "signed up" : "logged in"} successfully.`
-      : res.message,
-  };
-  showAlert.value = true;
-  setTimeout(() => {
-    showAlert.value = false;
-    if (!isSignup && res.success) router.push("/dashboard");
-  }, isSignup ? 2000 : 900);
-}
+
 function flip(mode: string) {
   flipClass.value =
     mode == "signup" ? "flip-card-first" : "flip-card-flipped-first";
