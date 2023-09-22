32a500bbc8148cb8af383ff6daf4a8b7443c82c7
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-22 15:18:26 +0330

    alert component added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view></router-view>
+  <template class="d-flex justify-center">
+    <router-view></router-view>
+    <alert />
+  </template>
 </template>
 
 <script lang="ts" setup>
-
+import alert from './components/containers/alert.vue';
 </script>
 
-<style></style>
+<style>
+
+</style>
