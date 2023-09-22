c6da2f00c45fbc5edc57289575feafd2808fa818
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-22 15:17:45 +0330

    alert component

---
+++ src/components/containers/alert.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-fade-transition>
+    <v-alert
+      density="comfortable"
+      class="fix-b mb-10 w-20 centered"
+      closable
+      v-if="alertStore.isShowing"
+      :type="(alertStore.type as any)"
+      :value="false"
+      elevation="10"
+    >
+      {{ alertStore.message }}
+    </v-alert>
+  </v-fade-transition>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useAlertsStore } from "@/stores/alert";
+const alertStore = useAlertsStore();
+</script>
+
+<style>
+
+</style>
