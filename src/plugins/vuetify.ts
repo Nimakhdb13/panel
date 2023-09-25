9d174e9bb6e93a425f207841e274d0d0a5ba325f
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-25 18:57:21 +0330

    adding light theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,36 @@
 import "vuetify/styles";
 import "@fortawesome/fontawesome-free/css/all.css"; // Ensure your project is capable of handling css files
 import { createVuetify } from "vuetify";
+import type { ThemeDefinition } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, fa } from "vuetify/iconsets/fa";
 
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#9C27B0",
+    "primary-darken-1": "#8E24AA",
+    "primary-darken-2": "#7B1FA2",
+    "primary-darken-3": "#6A1B9A",
+    "primary-darken-4": "#4A148C",
+    "back-1": "b6756a",
+    "back-2": "373771",
+    "back-3": "70416e",
+    "primary-lighten-1": "#3700B3",
+    "primary-lighten-2": "#BA68C8",
+    "primary-lighten-3": "#CE93D8",
+    "primary-lighten-4": "#E1BEE7",
+    secondary: "#5C6BC0",
+    "secondary-darken-1": "#018786",
+    error: "#FF1744",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};  
 export default createVuetify({
   components,
   directives,
@@ -13,6 +39,12 @@ export default createVuetify({
     aliases,
     sets: {
       fa,
+    },
+  },
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme
     },
   },
 });
