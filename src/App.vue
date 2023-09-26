5ebb822f2d16b4f7534d2f6d9ec6e7741ce646ad
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-26 08:44:49 +0330

    theme toggle component added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <theme-toggle></theme-toggle>
   <template class="d-flex justify-center">
     <router-view></router-view>
     <alert />
@@ -7,6 +8,7 @@
 
 <script lang="ts" setup>
 import alert from './components/containers/alert.vue';
+import themeToggle from './components/controls/themeToggle.vue';
 </script>
 
 <style>
