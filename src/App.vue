607d8816095194fed1cab6a4b8c5e98af243a16c
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-26 08:48:37 +0330

    setting background of app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <theme-toggle></theme-toggle>
-  <template class="d-flex justify-center">
-    <router-view></router-view>
-    <alert />
-  </template>
+  <div :class="$vuetify.theme.global.name == 'myCustomLightTheme' ? 'index-container-light' : 'index-container-dark'" >
+    <theme-toggle></theme-toggle>
+    <template class="d-flex justify-center">
+      <router-view></router-view>
+      <alert />
+    </template>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -12,5 +14,12 @@ import themeToggle from './components/controls/themeToggle.vue';
 </script>
 
 <style>
-
+.index-container-light{
+  height:100vh;
+  background: linear-gradient(110deg, rgb(var(--v-theme-back-1)) 0%, rgb(var(--v-theme-back-3)) 49%, rgb(var(--v-theme-back-1)) 100%);   
+}
+.index-container-dark{
+  height:100vh  ;
+  background: linear-gradient(110deg, rgb(var(--v-theme-back-1)) 0%, rgb(var(--v-theme-back-2)) 49%, rgb(var(--v-theme-back-3)) 100%); 
+}
 </style>
