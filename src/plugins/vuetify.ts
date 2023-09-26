361b50c96052f9a2c22138061b6add30e7fb00a3
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-26 08:34:32 +0330

    adding dark theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -11,6 +11,33 @@ const myCustomLightTheme: ThemeDefinition = {
   colors: {
     background: "#FFFFFF",
     surface: "#FFFFFF",
+    primary: "#58545B",
+    "primary-darken-1": "#58545B",
+    "primary-darken-2": "#58545B",
+    "primary-darken-3": "#58545B",
+    "primary-darken-4": "#58545B",
+    "back-1": "92DAD9",
+    "back-2": "DCEBEB",
+    "back-3": "F7E2E1",
+    "primary-lighten-1": "#58545B",
+    "primary-lighten-2": "#AC6869",
+    "primary-lighten-3": "#AC6869",
+    "primary-lighten-4": "#58545B",
+    secondary: "#ffffff",
+    "secondary-darken-1": "#018786",
+    texts : 'c47676',
+    error: "#FF1744",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};  
+
+const myCustomDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
     primary: "#9C27B0",
     "primary-darken-1": "#8E24AA",
     "primary-darken-2": "#7B1FA2",
@@ -19,12 +46,13 @@ const myCustomLightTheme: ThemeDefinition = {
     "back-1": "b6756a",
     "back-2": "373771",
     "back-3": "70416e",
+    texts : 'ffffff',
     "primary-lighten-1": "#3700B3",
     "primary-lighten-2": "#BA68C8",
     "primary-lighten-3": "#CE93D8",
     "primary-lighten-4": "#E1BEE7",
-    secondary: "#5C6BC0",
-    "secondary-darken-1": "#018786",
+    secondary: "#607D8B",
+    "secondary-darken-4": "#263238",
     error: "#FF1744",
     info: "#2196F3",
     success: "#4CAF50",
@@ -44,7 +72,8 @@ export default createVuetify({
   theme: {
     defaultTheme: "myCustomLightTheme",
     themes: {
-      myCustomLightTheme
+      myCustomLightTheme,
+      myCustomDarkTheme
     },
   },
 });
