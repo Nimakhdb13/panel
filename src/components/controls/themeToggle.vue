b46f7154e2b4676b4c97c399a9365d8182323787
Nimakhdb13 <shahinnima13@gmail.com>
2023-09-26 08:44:18 +0330

    theme toggle component

---
+++ src/components/controls/themeToggle.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-switch
+    v-model="isDark"
+    class="toggle-btn"
+    inset
+    inline
+    density="compact"
+    true-icon="fas fa-moon"
+    false-icon="fas fa-sun"
+  ></v-switch>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+import { useTheme } from "vuetify";
+let isDark = ref(false);
+let theme = useTheme();
+watch(isDark, (newIsDark) => {
+  theme.global.name.value = newIsDark ? "myCustomDarkTheme" : "myCustomLightTheme";
+})
+function toggle() {
+}
+</script>
+
+<style>
+.toggle-btn {
+  position: absolute !important;
+}
+</style>
