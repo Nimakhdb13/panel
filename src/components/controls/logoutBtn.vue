5cae519951418513c63caf634b09162073622a65
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-01 21:39:46 +0330

    logout dialog comp. added to logout Btn

--- src/components/controls/logoutBtn.vue
+++ src/components/controls/logoutBtn.vue
@@ -2,17 +2,22 @@
   <v-tooltip location="bottom" text="Logout">
     <template v-slot:activator="{ props }">
       <v-btn
-        @click="logout()"
+        @click="openLogutDialog"
         v-bind="props"
         icon="fas fa-sign-out"
       ></v-btn>
     </template>
   </v-tooltip>
+  <logout-dialog ref="logoutDialogRef"></logout-dialog>
 </template>
+
 <script lang="ts" setup>
-import router from "@/router";
-function logout() {
-  localStorage.removeItem("token");
-  router.push("/");
+import { ref } from "vue";
+import logoutDialog from "@/components/dialogs/logout.vue";
+
+let logoutDialogRef = ref(null);
+
+function openLogutDialog() {
+  if (logoutDialogRef.value) (logoutDialogRef.value as any).show();
 }
 </script>
