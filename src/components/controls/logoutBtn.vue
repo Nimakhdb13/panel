7f0f51b7570167fcb6757e1b2f23bb7612d2c058
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-01 15:23:20 +0330

    make logout button component

---
+++ src/components/controls/logoutBtn.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-btn @click="logout()" color="success">logout</v-btn>
+</template>
+<script lang="ts" setup>
+import router from "@/router";
+function logout() {
+  localStorage.removeItem("token");
+  router.push("/");
+}
+</script>
