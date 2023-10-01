6f4278e8a9ca5ab7370bf504ee652fb276c66d60
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-01 19:23:59 +0330

    adding logout btn to app var

--- src/components/containers/appBar.vue
+++ src/components/containers/appBar.vue
@@ -1,12 +1,13 @@
 <template>
 <v-app-bar class="px-2 bg-op-0" elevation="2"> 
-    <theme-toggle></theme-toggle>
+    <theme-toggle ></theme-toggle>
+    <logout-btn></logout-btn>
 </v-app-bar>
 </template>
 
 <script lang="ts" setup>
 import themeToggle from '../controls/themeToggle.vue';
-
+import logoutBtn from '../controls/logoutBtn.vue';
 </script>
 
 <style>
