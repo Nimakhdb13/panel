efcbf3c76089a4ed9fdcac3d7e6aea70553591bc
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-01 15:20:55 +0330

    adding app bar to app,adding toggle btn to app bar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template>
-  <div :class="$vuetify.theme.global.name == 'myCustomLightTheme' ? 'index-container-light' : 'index-container-dark'" >
-    <theme-toggle></theme-toggle>
-    <template class="d-flex justify-center">
-      <router-view></router-view>
-      <alert />
-    </template>
-  </div>
+  <v-app>
+    <div
+      class="f-h"
+      :class="
+        $vuetify.theme.global.name == 'myCustomLightTheme'
+          ? 'index-container-light'
+          : 'index-container-dark'
+      "
+    >
+      <template class="d-flex justify-center">
+        <appBar></appBar>
+        <router-view></router-view>
+        <alert />
+      </template>
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-import alert from './components/containers/alert.vue';
-import themeToggle from './components/controls/themeToggle.vue';
+import alert from "./components/containers/alert.vue";
+import appBar from "./components/containers/appBar.vue";
 </script>
 
 <style>
-.index-container-light{
-  height:100vh;
-  background: linear-gradient(110deg, rgb(var(--v-theme-back-1)) 0%, rgb(var(--v-theme-back-3)) 49%, rgb(var(--v-theme-back-1)) 100%);   
+.index-container-light {
+  background: linear-gradient(
+    110deg,
+    rgb(var(--v-theme-back-1)) 0%,
+    rgb(var(--v-theme-back-3)) 49%,
+    rgb(var(--v-theme-back-1)) 100%
+  );
 }
-.index-container-dark{
-  height:100vh  ;
-  background: linear-gradient(110deg, rgb(var(--v-theme-back-1)) 0%, rgb(var(--v-theme-back-2)) 49%, rgb(var(--v-theme-back-3)) 100%); 
+.index-container-dark {
+  background: linear-gradient(
+    110deg,
+    rgb(var(--v-theme-back-1)) 0%,
+    rgb(var(--v-theme-back-2)) 49%,
+    rgb(var(--v-theme-back-3)) 100%
+  );
 }
 </style>
---
+++ src/components/containers/appBar.vue
@@ -0,0 +1,13 @@
+<template>
+<v-app-bar class="px-2 bg-op-0" elevation="2"> 
+    <theme-toggle></theme-toggle>
+</v-app-bar>
+</template>
+
+<script lang="ts" setup>
+import themeToggle from '../controls/themeToggle.vue';
+
+</script>
+
+<style>
+</style>
