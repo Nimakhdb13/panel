aaa9772dc6f56ad2ab09b0df43d9a136e1b7fb33
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-04 14:05:06 +0330

    admin dashboard component added

---
+++ src/components/dashboards/adminDashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">role</th>
+        <th class="text-left">email</th>
+        <th class="text-left">phone</th>
+        <th class="text-left">isActive</th>
+        <th class="text-left">created at</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in users" :key="item.name">
+        <td>{{ item.username }}</td>
+        <td>{{ item.role }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.phone }}</td>
+        <td>{{ item.active   }}</td>
+        <td>{{ item.created_at }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script lang="ts" setup>
+import { UserApi } from "@/api/users/user";
+let users: any;
+new UserApi().getAllUsers().then((res) => {
+  users = res;
+});
+</script>
+
+<style></style>
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,8 +1,9 @@
 <template>
+  <adminDashboard></adminDashboard>
 </template>
 
 <script lang="ts" setup>
-
+import adminDashboard from "@/components/dashboards/adminDashboard.vue";
 </script>
 
 <style></style>
