ea659fd710353481c6e4cfd1aefb7b4012720d16
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-04 16:42:54 +0330

    progress bar added to users table when loading

--- src/components/dashboards/adminDashboard.vue
+++ src/components/dashboards/adminDashboard.vue
@@ -1,28 +1,36 @@
 <template>
   <v-row class="mt-5 justify-center">
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left" v-for="header in userHeaders" :key="header.value">{{header.text}}</th>
-        </tr>
-      </thead>
-      <tbody v-if="loaded">
-        <tr v-for="user in users" :key="user.name">
-          <td v-for="key in userHeaders" :key="key.value">{{ user[key.value] }}</td>
-          <td>
-            <v-btn min-width="0" min-height="0" icon="fa fa-trash" color="success" class="mx-2"></v-btn>
-            <v-btn color="success">promote</v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+    <div>
+      <v-table>
+        <thead>
+          <tr>
+            <th
+              width="200px"
+              class="text-center"
+              v-for="header in userHeaders"
+              :key="header.value"
+            >
+              {{ header.text }}
+            </th>
+          </tr>
+        </thead>
+        <tbody v-if="loaded">
+          <tr class="text-center" v-for="user in users" :key="user.name">
+            <td class="t" v-for="key in userHeaders" :key="key.value">
+              {{ user[key.value] }}
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+      <v-progress-linear indeterminate v-if="!loaded"></v-progress-linear>
+    </div>
   </v-row>
 </template>
 
 <script lang="ts" setup>
 import { UserApi } from "@/api/users/user";
 import { ref } from "vue";
-import userHeaders from "@/assets/headers/users"
+import userHeaders from "@/assets/headers/users";
 let users = ref({} as any);
 let loaded = ref(false);
 new UserApi().getAllUsers().then((res) => {
@@ -31,4 +39,8 @@ new UserApi().getAllUsers().then((res) => {
 });
 </script>
 
-<style></style>
+<style>
+.t{
+  max-width: 200px;
+}
+</style>
