a524605e6caff94242a06a44351dedd740e146fb
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-04 17:11:26 +0330

    show date number latin instead of persian

--- src/composibles/formatDates.ts
+++ src/composibles/formatDates.ts
@@ -4,12 +4,12 @@ export function useJalaliDate(objectWithDate: any) {
   if (objectWithDate.length)
     return objectWithDate.map((element: any) => {
       element.created_at = new Date(element.created_at).toLocaleDateString(
-        "fa-IR"
+        "fa-IR-u-nu-latn"
       );
     });
   else {
     objectWithDate.created_at = new Date(objectWithDate.created_at).toLocaleDateString(
-      "fa-IR"
+      "fa-IR-u-nu-latn"
     );
   }
 }
