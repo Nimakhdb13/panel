287fe9c54c52e0838a8b11ce4651fd706fb7f981
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-04 16:10:21 +0330

    add header dir. and users headers, made user table dynamic by user headers

---
+++ src/assets/headers/users.ts
@@ -0,0 +1,27 @@
+export default [
+    {
+        text:"User Name",
+        value:"username"
+    },
+    {
+        text:"Phone Number",
+        value:"phone"
+    },
+    {
+        text:"Email",
+        value:"email"
+    },
+    {
+        text:"Role",
+        value:"role"
+    },
+    {
+        text:"Create Date",
+        value:"created_at"
+    },
+    {
+        text:"Is Active",
+        value:"active"
+    }
+
+]
--- src/components/dashboards/adminDashboard.vue
+++ src/components/dashboards/adminDashboard.vue
@@ -1,33 +1,33 @@
 <template>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">Name</th>
-        <th class="text-left">role</th>
-        <th class="text-left">email</th>
-        <th class="text-left">phone</th>
-        <th class="text-left">isActive</th>
-        <th class="text-left">created at</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in users" :key="item.name">
-        <td>{{ item.username }}</td>
-        <td>{{ item.role }}</td>
-        <td>{{ item.email }}</td>
-        <td>{{ item.phone }}</td>
-        <td>{{ item.active   }}</td>
-        <td>{{ item.created_at }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <v-row class="mt-5 justify-center">
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left" v-for="header in userHeaders" :key="header.value">{{header.text}}</th>
+        </tr>
+      </thead>
+      <tbody v-if="loaded">
+        <tr v-for="user in users" :key="user.name">
+          <td v-for="key in userHeaders" :key="key.value">{{ user[key.value] }}</td>
+          <td>
+            <v-btn min-width="0" min-height="0" icon="fa fa-trash" color="success" class="mx-2"></v-btn>
+            <v-btn color="success">promote</v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-row>
 </template>
 
 <script lang="ts" setup>
 import { UserApi } from "@/api/users/user";
-let users: any;
+import { ref } from "vue";
+import userHeaders from "@/assets/headers/users"
+let users = ref({} as any);
+let loaded = ref(false);
 new UserApi().getAllUsers().then((res) => {
-  users = res;
+  users.value = res;
+  loaded.value = true;
 });
 </script>
 
