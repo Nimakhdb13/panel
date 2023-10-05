df7e4ba7fec08e9fc22506b2751f219512366f6b
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-05 16:49:33 +0330

    show dashboard based on user role

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,9 +1,14 @@
 <template>
-  <adminDashboard></adminDashboard>
+  <adminDashboard v-if="authStore.role == 'admin'"></adminDashboard>
+  <UserDashboard v-if="authStore.role == 'user'"></UserDashboard>
 </template>
 
 <script lang="ts" setup>
+import { useAuthStore } from "@/stores/auth";
 import adminDashboard from "@/components/dashboards/adminDashboard.vue";
+import UserDashboard from "@/components/dashboards/userDashboard.vue";
+let authStore = useAuthStore();
+
 </script>
 
 <style></style>
