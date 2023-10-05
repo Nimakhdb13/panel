5366259865102df44afcfed9b853981c52101f6c
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-05 16:48:04 +0330

    store username and role when login

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,14 +7,21 @@ export const useAuthStore = defineStore("auth", () => {
   let token = ref("");
   let userName = ref("");
   let role = ref("");
-  function login(_token: string) {
+
+  function login(_token: string, _role: string, _username: string) {
     isLoggedIn.value = true;
     token.value = _token;
+    role.value = _role;
+    userName.value = _username;
   }
+
   function reset() {
     isLoggedIn.value = false;
     token.value = "";
+    role.value = "";
+    userName.value = "";
   }
+  
   function init() {
     token.value = localStorage.getItem("token") as string;
     if (token.value) {
