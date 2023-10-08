9ce3bd32138aac94da6027427bfdb17e78b1a224
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-08 16:26:04 +0330

    form components dir and new task form component

--- src/components/dialogs/newTask.vue
+++ src/components/dialogs/newTask.vue
@@ -1,31 +1,36 @@
 <template>
-    <v-dialog
-      persistent
-      :width="$vuetify.display.mdAndUp ? '30vw' : '90vw'"
-      v-model="showDialog"
-    >
-      <template v-slot:default="{ isActive }">
-        <v-card title="New Task">
-          <v-card-actions class="d-flex justify-space-around">
-            <v-btn text="Cancel" @click="close"></v-btn>
-            <v-btn text="Add" append-icon="fa-regular fa-plus" color="success"></v-btn>
-          </v-card-actions>
-        </v-card>
-      </template>
-    </v-dialog>
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref } from "vue";
-  let showDialog = ref(false);
-  function show() {
-    showDialog.value = true;
-  }
-  function close() {
-    showDialog.value = false;
-  }
-  defineExpose({
-    show,
-  });
-  </script>
-  
+  <v-dialog
+    persistent
+    :width="$vuetify.display.mdAndUp ? '30vw' : '90vw'"
+    v-model="showDialog"
+  >
+    <template v-slot:default="{ isActive }">
+      <v-card title="New Task">
+        <newTaskForm></newTaskForm>
+        <v-card-actions class="d-flex justify-space-around">
+          <v-btn text="Cancel" @click="close"></v-btn>
+          <v-btn
+            text="Add"
+            append-icon="fa-regular fa-plus"
+            color="success"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import newTaskForm from "@/components/forms/newTask.vue";
+import { ref } from "vue";
+let showDialog = ref(false);
+function show() {
+  showDialog.value = true;
+}
+function close() {
+  showDialog.value = false;
+}
+defineExpose({
+  show,
+});
+</script>
---
+++ src/components/forms/newTask.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-sheet class="mx-4 bg-op-0">
+    <v-form v-model="isFormValid" @submit.prevent>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+import _rules from "../../assets/rules";
+import { ref } from "vue";
+let isFormValid = ref(false);
+</script>
+
+<style></style>
