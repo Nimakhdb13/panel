7d42f4682f15ad449b7b268a922e12df9d8df367
Nimakhdb13 <shahinnima13@gmail.com>
2023-10-08 19:00:51 +0330

    add task function to call api

--- src/components/dialogs/newTask.vue
+++ src/components/dialogs/newTask.vue
@@ -6,13 +6,14 @@
   >
     <template v-slot:default="{ isActive }">
       <v-card title="New Task">
-        <newTaskForm></newTaskForm>
+        <newTaskForm ref="newTaskFormRef"></newTaskForm>
         <v-card-actions class="d-flex justify-space-around">
           <v-btn text="Cancel" @click="close"></v-btn>
           <v-btn
             text="Add"
             append-icon="fa-regular fa-plus"
             color="success"
+            @click="add"
           ></v-btn>
         </v-card-actions>
       </v-card>
@@ -21,14 +22,29 @@
 </template>
 
 <script lang="ts" setup>
+import { TaskApi } from "@/api/tasks/task";
 import newTaskForm from "@/components/forms/newTask.vue";
+import { useAlertsStore } from "@/stores/alert";
 import { ref } from "vue";
+let alertStore = useAlertsStore();
+let newTaskFormRef = ref(null);
 let showDialog = ref(false);
 function show() {
   showDialog.value = true;
 }
 function close() {
   showDialog.value = false;
+}
+function add(){
+  if(newTaskFormRef.value)
+  new TaskApi().createTask((newTaskFormRef.value as any).newTask).then((res)=>{
+    alertStore.$patch({
+      message: res.message,
+      type:"success"    
+    })
+    alertStore.showAlert(); 
+    close();
+  })
 }
 defineExpose({
   show,
